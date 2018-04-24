78bca60c6ba90038a107d52d85c94f16afce5a44
e10ulen <e10ulen@ymobile.ne.jp>
2018-04-25 05:20:08 +0900

    [Commit]2018/04/25 05:20 update tui

--- tui/main.go
+++ tui/main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/comail/colog"
+	tui "github.com/marcusolsson/tui-go"
+)
+
+var (
+	player   = "vipper"
+	playerLv = "1"
+	playerHP = "100"
+	playerMP = "50"
+	pATK     = "10"
+	pDEF     = "10"
+	pINT     = "10"
+	pAGI     = "10"
+	pVIT     = "10"
+	pLUC     = "10"
+)
+
+type getText struct {
+	label *tui.Label
+}
 
 func main() {
-	fmt.Println("vim-go")
-	tui.NewLavel
+	colog.Register()
+	menuBar := tui.NewVBox()
+	menuBar.SetBorder(true)
+	addMenu()
+	menuBar.Append(tui.NewLabel("test"))
+	mainBar := tui.NewVBox()
+	mainBar.Append(tui.NewLabel("test"))
+	mainBar.SetBorder(true)
+	root := tui.NewHBox(menuBar, mainBar)
+	ui, err := tui.New(root)
+	if err != nil {
+		log.Print("w:", err)
+	}
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	if err := ui.Run(); err != nil {
+		log.Print("e: RunError ", err)
+	}
+}
+func addMenu(*getText) {
+	menuBar.Append(label("addMenu"))
 }
