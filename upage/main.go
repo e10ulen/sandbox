deaf2361a3e912047985ab44fd8262dd49681ba8
e10ulen <slcb.sena@gmail.com>
2020-07-02 16:13:34 +0900

    DB接続テスト

--- upage/main.go
+++ upage/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,14 @@ import (
 	"github.com/comail/colog"
 
 	"github.com/PuerkitoBio/goquery"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+func execDB(db *sql.DB, q string) {
+	if _, err := db.Exec(q); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	//	ロガー
@@ -21,8 +29,35 @@ func main() {
 	if err != nil {
 		log.Print("e:")
 	}
+	//	DataBase接続設定
+	db, err := sql.Open("sqlite3", "./test.db")
+	//	DB接続時エラー
+	if err != nil {
+		log.Fatal(err)
+	}
+	//	DB作成
+	q := `
+		CREATE TABLE update (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			body VARCHAR(255) NOT NULL,
+			created_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))
+		)
+	`
+	//	DB呼び出し
+	execDB(db, q)
+
+	//	DBテスト書き込み
+	q = `
+        INSERT INTO update (body)
+        VALUES ('body1'), ('body2')
+    `
+	//	DBテスト書き込みのための呼び出し
+	execDB(db, q)
+	//	DBを閉じる
+	db.Close()
+
+	//	通常処理
 	defer res.Body.Close()
-	var sliceURL []string
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Print("e: url scrapping failed", err)
@@ -35,10 +70,6 @@ func main() {
 		url, _ := s.Attr("href")
 		//	置換で間違って入ってる、mailto:sageを除去。
 		replaceURL := strings.Replace(url, "mailto:sage", "", -1)
-
-		sliceURL = append(sliceURL, replaceURL)
 		fmt.Printf("Nun:%d URL:%s\n", index, replaceURL)
-
 	})
-	fmt.Printf("Slice:%v", len(sliceURL))
 }
