22481742d98fc2950dd3f6980b4d3659ada5fb68
e10ulen <slcb.sena@gmail.com>
2020-07-02 11:54:18 +0900

    想定外の文字列を排出しないようにした

--- upage/main.go
+++ upage/main.go
@@ -102,7 +102,6 @@ func main() {
 	selection := doc.Find("dl.thread")
 	innerselection := selection.Find("a")
 	innerselection.Each(func(index int, s *goquery.Selection) {
-		utfcheck := s.Find("dd").Text
 		// URLを取得する
 		//	URL取得部分
 		//		utfcheck := transform.NewReader(utfcheck, japanese.ShiftJIS.NewDecoder())
@@ -110,6 +109,6 @@ func main() {
 		//	置換で間違って入ってる、mailto:sageを除去。
 		replaceURL := strings.Replace(url, "mailto:sage", "", -1)
 		fmt.Println(replaceURL)
-		fmt.Println(utfcheck)
+
 	})
 }
