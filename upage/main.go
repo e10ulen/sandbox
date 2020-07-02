fa09de4f56f60bff1bab2434ecab00d384381ecc
e10ulen <slcb.sena@gmail.com>
2020-07-02 17:46:49 +0900

    DBに書き込みがうまく行かない。

--- upage/main.go
+++ upage/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/comail/colog"
 
@@ -13,63 +15,67 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func execDB(db *sql.DB, q string) {
-	if _, err := db.Exec(q); err != nil {
-		log.Fatal(err)
-	}
+//	スレッドURLv
+var threadURL = "http://dawnlight.ovh/test/read.cgi/viptext/1520663900/"
+
+//	DBPath
+const dbPath = "./update.db"
+
+//	データ格納
+//	|書き込み番号|書き込まれたURL|
+type Update struct {
+	number int
+	url    string
 }
 
 func main() {
 	//	ロガー
 	colog.Register()
-	//	スレッドURL指定
-	url := "http://dawnlight.ovh/test/read.cgi/viptext/1520663900/"
-	res, err := http.Get(url)
+	//	データベースOpen
+	DBConnnect, _ := sql.Open("sqlite3", dbPath)
+	//	データベースClose
+	defer DBConnnect.Close()
+	//	スレッドテーブル作成
+	if file, err := os.Stat(dbPath); os.IsNotExist(err) || file.IsDir() {
+		cmd := `CREATE TABLE IF NOT EXISTS "threadUP"(
+			"number" INTEGER PRIMARY KEY,
+			"url" VARCHAR(255))`
+		//	cmdを実行する。リターンは受け取らない。
+		_, err := DBConnnect.Exec(cmd)
+		//	データベース用エラーハンドリング
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	res, err := http.Get(threadURL)
 	//	スレッド取得に失敗した際のエラー
 	if err != nil {
 		log.Print("e:")
 	}
-	//	DataBase接続設定
-	db, err := sql.Open("sqlite3", "./test.db")
-	//	DB接続時エラー
-	if err != nil {
-		log.Fatal(err)
-	}
-	//	DB作成
-	q := `
-		CREATE TABLE update (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			body VARCHAR(255) NOT NULL,
-			created_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))
-		)
-	`
-	//	DB呼び出し
-	execDB(db, q)
-
-	//	DBテスト書き込み
-	q = `
-        INSERT INTO update (body)
-        VALUES ('body1'), ('body2')
-    `
-	//	DBテスト書き込みのための呼び出し
-	execDB(db, q)
-	//	DBを閉じる
-	db.Close()
-
+	log.Print("d: %v", time.Now())
 	//	通常処理
 	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Print("e: url scrapping failed", err)
 	}
-	//一個ずつの投稿を取得する
+	//	データベース挿入処理
+	cmd := "UPDATE threadUP ( number, url) VALUES(?,?)"
+	//	一個ずつの投稿を取得する
 	selection := doc.Find("dl.thread")
 	innerselection := selection.Find("a")
 	innerselection.Each(func(index int, s *goquery.Selection) {
 		//	URL取得部分
-		url, _ := s.Attr("href")
+		updateurl, _ := s.Attr("href")
+
 		//	置換で間違って入ってる、mailto:sageを除去。
-		replaceURL := strings.Replace(url, "mailto:sage", "", -1)
+		replaceURL := strings.Replace(updateurl, "mailto:sage", "", -1)
 		fmt.Printf("Nun:%d URL:%s\n", index, replaceURL)
+		_, err = DBConnnect.Exec(cmd, index, replaceURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	})
 }
